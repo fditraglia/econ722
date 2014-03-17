da759b942c56e866151af3fa982a1c36be945385
Francis DiTraglia <fditra@sas.upenn.edu>
2014-03-17 16:06:13 -0400

    added R code for Figure 4.2 from the book

--- AIC_vs_BIC_plots.R
+++ AIC_vs_BIC_plots.R
@@ -14,3 +14,43 @@ y.min <- min(min(pBIC), min(pAIC))
 plot(mu, pAIC, ylim = c(y.min, y.max), xlab = expression(mu), ylab = 'Probability of Selecting M1', type = 'l', col = 'red', lwd = 2)
 points(mu, pBIC, type = 'l', col = 'blue', lty = 2, lwd = 2)
 legend('bottomright', legend = c('AIC', 'BIC'), lty = c(1,2), col = c('red', 'blue'), lwd = c(2,2))
+
+
+
+
+
+#Figure 4.2
+d.AIC <- function(n.T){return(2)}
+d.BIC <- function(n.T){return(log(n.T))}
+d.Hodge <- function(n.T){return(sqrt(n.T))}
+
+risk <- function(n.T, mu, d.T){
+  a <- -sqrt(d.T(n.T)) - sqrt(n.T) * mu
+  b <- sqrt(d.T(n.T)) - sqrt(n.T) * mu
+  first.term <-  a * dnorm(a) - b * dnorm(b) + (pnorm(b) - pnorm(a))
+  second.term <- n.T * mu^2 * (pnorm(b) - pnorm(a))
+  return(1 - first.term + second.term)
+}
+
+risk2 <- function(n.T, mu, d.T){
+  a <- -sqrt(d.T(n.T)) - sqrt(n.T) * mu
+  b <- sqrt(d.T(n.T)) - sqrt(n.T) * mu
+  first.term <- b * dnorm(b) - a * dnorm(a) 
+  second.term <- (n.T * mu^2 - 1) * (pnorm(b) - pnorm(a))
+  return(1 + first.term + second.term)
+}
+
+mu <- seq(from = -2, to = 2, by = 0.01)
+sample.size <- 200
+risk.mu <- cbind(risk(sample.size, mu, d.AIC), 
+                 risk(sample.size, mu, d.BIC), 
+                 risk(sample.size, mu, d.Hodge))
+matplot(mu, risk.mu, ylab = 'Risk', xlab = expression(mu), type = 'l', lwd = 2, col = 'black')
+
+max(risk(sample.size, mu, d.AIC))
+max(risk(sample.size, mu, d.BIC))
+max(risk(sample.size, mu, d.Hodge))
+
+all.equal(risk2(sample.size, mu, d.AIC), risk(sample.size, mu, d.AIC))
+all.equal(risk2(sample.size, mu, d.BIC), risk(sample.size, mu, d.BIC))
+all.equal(risk2(sample.size, mu, d.Hodge), risk(sample.size, mu, d.Hodge))
