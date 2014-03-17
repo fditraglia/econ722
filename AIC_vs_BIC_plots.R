0ed66d33df75e6daa5b9576c02af4415e7b9bb35
Francis DiTraglia <fditra@sas.upenn.edu>
2014-03-17 16:08:29 -0400

    removed some preliminary code
    risk2 is the simplified version of the risk function: I checked that it
    gives the sample example as risk and as the book so I no longer need
    the original version of the function risk which I've now replaced by
    risk2

--- AIC_vs_BIC_plots.R
+++ AIC_vs_BIC_plots.R
@@ -27,14 +27,6 @@ d.Hodge <- function(n.T){return(sqrt(n.T))}
 risk <- function(n.T, mu, d.T){
   a <- -sqrt(d.T(n.T)) - sqrt(n.T) * mu
   b <- sqrt(d.T(n.T)) - sqrt(n.T) * mu
-  first.term <-  a * dnorm(a) - b * dnorm(b) + (pnorm(b) - pnorm(a))
-  second.term <- n.T * mu^2 * (pnorm(b) - pnorm(a))
-  return(1 - first.term + second.term)
-}
-
-risk2 <- function(n.T, mu, d.T){
-  a <- -sqrt(d.T(n.T)) - sqrt(n.T) * mu
-  b <- sqrt(d.T(n.T)) - sqrt(n.T) * mu
   first.term <- b * dnorm(b) - a * dnorm(a) 
   second.term <- (n.T * mu^2 - 1) * (pnorm(b) - pnorm(a))
   return(1 + first.term + second.term)
@@ -51,6 +43,3 @@ max(risk(sample.size, mu, d.AIC))
 max(risk(sample.size, mu, d.BIC))
 max(risk(sample.size, mu, d.Hodge))
 
-all.equal(risk2(sample.size, mu, d.AIC), risk(sample.size, mu, d.AIC))
-all.equal(risk2(sample.size, mu, d.BIC), risk(sample.size, mu, d.BIC))
-all.equal(risk2(sample.size, mu, d.Hodge), risk(sample.size, mu, d.Hodge))
